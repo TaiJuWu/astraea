5c9e7aac2cc5daba5512af731fd055c849a0d54b
TaiJu Wu <tjwu1217@gmail.com>
2025-09-05 22:06:21 +0800

    Use abs path instead of /tmp

--- docker/start_controller.sh
+++ docker/start_controller.sh
@@ -227,11 +227,11 @@ setPropertyIfEmpty "transaction.state.log.replication.factor" "1"
 setPropertyIfEmpty "offsets.topic.replication.factor" "1"
 setPropertyIfEmpty "transaction.state.log.min.isr" "1"
 setPropertyIfEmpty "min.insync.replicas" "1"
-setPropertyIfEmpty "log.dirs" "/tmp/${META_FOLDER}"
+setPropertyIfEmpty "log.dirs" "${META_FOLDER}"
 
 metaMountCommand=""
 if [[ -n "$META_FOLDER" ]]; then
-  metaMountCommand="-v /tmp/$META_FOLDER:/tmp/${META_FOLDER}:Z"
+  metaMountCommand="-v $META_FOLDER:/tmp/${META_FOLDER}:Z"
 fi
 
 release_version=""
@@ -253,7 +253,7 @@ docker run -d --init \
   -e KAFKA_JMX_OPTS="$JMX_OPTS" \
   -e KAFKA_OPTS="-javaagent:/opt/jmx_exporter/jmx_prometheus_javaagent-${EXPORTER_VERSION}.jar=$EXPORTER_PORT:$JMX_CONFIG_FILE_IN_CONTAINER_PATH" \
   -v $CONTROLLER_PROPERTIES:/tmp/controller.properties:ro,Z \
-  -v /tmp/${META_FOLDER}/logs:/opt/kafka/logs \
+  -v ${META_FOLDER}/logs:/opt/kafka/logs \
   -v ${LOG4j2_YAML}:/opt/kafka/config/log4j2.yaml \
   $(generateJmxConfigMountCommand) \
   $metaMountCommand \
